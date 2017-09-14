3535eae7b87ce4c00a7163b6176f0a331adf92b2
Nathan N <Nitharios@users.noreply.github.com>
2017-09-13 14:24:51 -1000

    All tests pass

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,12 @@
 'use strict';
+
 module.exports = function() {
-  
+  var hiddenNum;
+
+  return function secretNumber() {
+    if (hiddenNum === undefined) {
+      hiddenNum = Math.random() * 1000000;
+    }
+    return hiddenNum;
+  };
 };
